ff658a01fe9691f113b4329b46f53fd7df0eda90
wangmingli <wangmingli@autohome.com.cn>
2018-06-12 19:34:46 +0800

    旅旅游项目初始化

---
+++ index.ios.js
@@ -0,0 +1,74 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+import Home from "./src/Home"
+import TabNavigator from "react-native-tab-navigator";
+
+export default class rn0525 extends Component {
+  constructor(props){
+    super(props);
+    this.state.selectedTab ="Home";
+  }
+  render() {
+    return (
+      <View>
+        <Home />
+        <TabNavigator>
+          <TabNavigator.Item 
+            selected={this.state.selectedTab === "Home"}
+            title="首页"
+            titleStyle={styles.tabText}
+            selectedTitleStyle={styles.selectedTabText}
+            renderIcon={() => 
+              <Image style={styles.icon} source={require("./images/tabbar_home.png")} />
+            }
+            renderSelectedIcon={() => 
+            <Image style={styles.icon} source={require("./images/tabbar-home-selected.png")}/>
+            }
+            onPress={() => 
+              this.setState({
+                selectedTab: 'Home'
+              })
+            }
+          >
+          <View>
+            <Home />
+          </View>
+          </TabNavigator.Item>
+        </TabNavigator>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('rn0525', () => rn0525);
---
+++ src/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import {View, Text} from "react-native";
+import { TabNavigator } from "react-navigation";
+
+export default class rn0525 extends Component {
+  render() {
+    return (
+      <View>
+          <Text>首页</Text>
+      </View>
+    );
+  }
+}
