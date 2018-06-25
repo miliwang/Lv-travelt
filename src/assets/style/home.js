bd3d0234b3479e3dc95b649e995d6b70ed272725
wangmingli <wangmingli@autohome.com.cn>
2018-06-25 18:42:13 +0800

    资源添加:art:

--- src/assets/style/home.js
+++ src/assets/style/home.js
@@ -5,11 +5,14 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "100%",
         marginTop: 20,
+        flexDirection: "row"
     },
     topBar: {
         width: "100%",
         height: 50,
-        position: "relative"
+        position: "relative",
+        alignItems: "center",
+        justifyContent:"center"
     },
     "top-bar-bg": {
         width: "100%",
@@ -23,5 +26,18 @@ const styles = StyleSheet.create({
         position: "absolute",
         top: 0,
         left: 0
+    },
+    "top-bar-search-view":{
+        backgroundColor: "#3ea290",
+        flex: 1,
+        borderRadius: 20
+    },
+    "searchIpt":{
+        height: 28,
+        lineHeight: 28,
+        width: "100%",
+        fontSize: 14,
+        paddingLeft: 10
     }
 });
+module.exports = styles;
